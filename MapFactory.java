a7bca66612d777c0d20c67fd66726633428fc80b
unknown <alejandraviles2005@gmail.com>
2025-03-13 07:01:31 -0600

    Manejo del MapFactory para la generación de los diferentes maps.

--- MapFactory.java
+++ MapFactory.java
@@ -1,6 +1,17 @@
+/* Autora: */
+
+import java.io.*;
+import java.util.*;
 @SuppressWarnings("rawtypes")
 
 public class MapFactory {
-    
+    public static Map<String, Pokemon> crearMap(int opcion) {
+        return switch (opcion) {
+            case 1 -> new HashMap<>();
+            case 2 -> new TreeMap<>();
+            case 3 -> new LinkedHashMap<>();
+            default -> throw new IllegalArgumentException("Opción inválida.");
+        };
+    }
 
 }
